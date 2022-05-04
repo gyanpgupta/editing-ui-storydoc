3a9f5dc06a2ec8fc316f44935f638637d0a675e3
gyanpgupta <guptagyan58@gmail.com>
2022-05-04 18:10:05 +0530

    inital setup and utils

---
+++ src/utils/AllIcons.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PieChartIcon from '@mui/icons-material/PieChart';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import AddIcCallIcon from '@mui/icons-material/AddIcCall';
+import AddToDriveIcon from '@mui/icons-material/AddToDrive';
+import AnnouncementIcon from '@mui/icons-material/Announcement';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import BatteryChargingFullIcon from '@mui/icons-material/BatteryChargingFull';
+
+export const iconData = [
+  <FavoriteIcon />,
+  <PieChartIcon />,
+  <ThumbUpIcon />,
+  <ThumbDownIcon />,
+  <EmojiEmotionsIcon />,
+  <AccessAlarmIcon />,
+  <AddAPhotoIcon />,
+  <AddIcCallIcon />,
+  <AddToDriveIcon />,
+  <AnnouncementIcon />,
+  <ArrowCircleLeftIcon />,
+  <BatteryChargingFullIcon />
+]
---
+++ src/utils/InitalIcons.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PieChartIcon from '@mui/icons-material/PieChart';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
+export const initialIcons = [
+  {
+    text: "This is Favourite",
+    icon: <FavoriteIcon />,
+  },
+  {
+    text: "This is Pie Chart",
+    icon: <PieChartIcon />,
+  },
+  {
+    text: "This is Like",
+    icon: <ThumbUpIcon />,
+  }
+]
